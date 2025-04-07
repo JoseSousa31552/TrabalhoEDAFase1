40b38e091880f298755def1315ec2054d127a4be
JoseSousa31552 <a31552@alunos.ipca.pt>
2025-04-07 17:08:57 +0100

    Update ASSFunco.h
    adição da assinatura e troca do tipo da função libertar antena de void para antena*

--- ASSFunco.h
+++ ASSFunco.h
@@ -1,4 +1,11 @@
 #pragma once
+/*****************************************************************//**
+ * \file   ASSFunco.h
+ * \brief  Assinatura das funções
+ * 
+ * \author José
+ * \date   April 2025
+ *********************************************************************/
 #include "Structs.h"
 
 
@@ -8,9 +15,9 @@ antena* procurarantena(antena* h, int l, int c);
 antena* insereOrdenado(antena* inicio, antena* novo);
 antena* removerantena(antena* h, int linha, int coluna);
 void mostrarMatrizAntenas(antena* lista_antenas);
-void libertarListaAntenas(antena* lista);
+antena* libertarListaAntenas(antena* lista);
 nefasto* criarEfeitoNefasto(int x, int y);
 nefasto* calcularEfeitoNefastoFinal(antena* lista);
 void mostrarMatrizNefastos(antena* lista_antenas, nefasto* lista_nefastos);
 void libertarListaNefasto(nefasto* lnefasto);
-
+int efeitoJaExiste(nefasto* lista, int x, int y);
